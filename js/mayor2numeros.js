0a8435c4a3238ea0c4abcde2f1e63d4ceb71eac7
romulofull <romulo.castro@gmail.com>
2024-07-29 23:03:05 -0500

    ParseInt

--- js/mayor2numeros.js
+++ js/mayor2numeros.js
@@ -1,5 +1,5 @@
-var valor1 = window.prompt ("Ingrese un numero : ");
-var valor2 = window.prompt ("Ingrese un numero : ");
+var valor1 = parseInt(window.prompt ("Ingrese un numero : "));
+var valor2 = parseInt(window.prompt ("Ingrese un numero : "));
 
 if (valor1 > valor2) {
 alert ("El mayor es el valor " + valor1);
