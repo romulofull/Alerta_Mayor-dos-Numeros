a9032b4a110a6340bddbcd667f697301ab3d1372
romulofull <romulo.castro@gmail.com>
2024-07-29 22:53:03 -0500

    Hecho

---
+++ js/mayor2numeros.js
@@ -0,0 +1,9 @@
+var valor1 = window.prompt ("Ingrese un numero : ");
+var valor2 = window.prompt ("Ingrese un numero : ");
+
+if (valor1 > valor2) {
+alert ("El mayor es el valor " + valor1);
+}
+else {
+alert ("El mayor es el valor " + valor2);
+}
